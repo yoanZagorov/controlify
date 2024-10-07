946016d58db8e5db9ec1ad0efd0b7fbb7a18b2ac
Yoan Zagorov <yoan.zagorov@gmail.com>
2024-10-07 10:14:01 +0300

    Initial commit - initalize app and set up file structure

---
+++ services/router.jsx
@@ -0,0 +1,11 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<h1>Hello there!</h1>}/>
+));
+
+export default router;
