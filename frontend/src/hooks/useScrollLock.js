56ec3a90a5939a02bb913a83c016a4f283a78651
Yoan Zagorov <yoan.zagorov@gmail.com>
2025-02-20 15:55:30 +0200

    Testing: PWA iOS improved support

--- frontend/src/hooks/useScrollLock.js
+++ frontend/src/hooks/useScrollLock.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function useScrollLock(isLocked) {
   useEffect(() => {
     if (isLocked) {
-      document.body.style.overflow + "hidden";
+      document.body.style.overflow = "hidden";
     }
 
     return () => {
